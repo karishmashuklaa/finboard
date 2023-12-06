f728fbfae0fa5fd87ab4ecdc0c1bc0dfcc0191ee
karishmas6 <carishmashukla28@gmail.com>
2023-12-06 23:35:49 +0530

    feat: sample data

---
+++ src/data/chartData.ts
@@ -0,0 +1,23 @@
+export const netProfit = [
+    { name: 'Jan', profit: 3000, revenue: 5000 },
+    { name: 'Feb', profit: 3500, revenue: 5500 },
+    { name: 'Mar', profit: 4000, revenue: 6000 },
+    { name: 'Apr', profit: 3800, revenue: 6500 },
+    { name: 'May', profit: 4000, revenue: 7000 },
+    { name: 'Jun', profit: 5500, revenue: 7500 },
+];
+
+export const pieData = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+
+]
+
+export const turnover = [
+    { name: 'Jan', 'Non Operational Expenses': 5000, 'Operational Expenses': 2000 },
+    { name: 'Feb', 'Non Operational Expenses': 5500, 'Operational Expenses': 2100 },
+    { name: 'Mar', 'Non Operational Expenses': 1100, 'Operational Expenses': 1200 },
+    { name: 'Apr', 'Non Operational Expenses': 800, 'Operational Expenses': 2300 },
+    { name: 'May', 'Non Operational Expenses': 3300, 'Operational Expenses': 2000 },
+];
+
