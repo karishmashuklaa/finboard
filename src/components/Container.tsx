97519c673af9da45bbe0105d3fe9d30cd981be44
karishmas6 <carishmashukla28@gmail.com>
2023-12-06 23:35:26 +0530

    feat: layout components and primitives

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const Container = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    height: '100%',
+    margin: '1rem',
+    marginTop: '-1.5rem'
+});
+
+export default Container;
---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,48 @@
+import Navbar from "@/scenes/navbar/index";
+import { Typography, useTheme } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+
+const RootLayout = () => {
+  const { palette } = useTheme();
+
+  return (
+    <>
+      <header>
+        <Box
+          width='100%'
+          height='100%'
+          padding="1rem 2rem 4rem 2rem"
+        >
+          <Navbar />
+        </Box>
+      </header>
+      <main
+        style={{
+          padding: '0 2rem',
+          minHeight: 'calc(100vh - 10rem)',
+        }}
+      >
+        <Outlet />
+      </main>
+      <footer
+        style={{
+          padding: '1rem 2rem',
+          backgroundColor: palette.grey[200],
+          marginTop: '2rem',
+        }}
+      >
+        <Typography
+          variant='body2'
+          align='center'
+          color={palette.grey[800]}
+          component='p'
+        >
+          &copy; {new Date().getFullYear()} - All rights reserved
+        </Typography>
+      </footer>
+    </>
+  )
+}
+
+export default RootLayout;
