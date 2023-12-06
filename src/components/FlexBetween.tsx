fa9e7e49cd96d5a8c6351f9e368d24e0aa2d9c0b
karishmas6 <carishmashukla28@gmail.com>
2023-12-06 23:53:32 +0530

    feat(ui/ux): mobile responsiveness

--- src/components/FlexBetween.tsx
+++ src/components/FlexBetween.tsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-const FlexBetween = styled(Box)({
+const FlexBetween = styled(Box)(({ theme }) => ({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  
-});
+
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+}));
 
 export default FlexBetween;
